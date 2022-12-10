92d70d3eaa67f032dd3d8f1e9072157b9ed96d03
SiskaNurawalia <90376230+SiskaNurawalia@users.noreply.github.com>
2022-12-10 04:02:27 -0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-import './article-list.js';
-import articles from './articles.js';
- 
-const articleListElement = document.createElement('article-list');
-articleListElement.articles = articles;
- 
-document.body.appendChild(articleListElement);
